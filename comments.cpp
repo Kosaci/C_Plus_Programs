9d428ae6f760bd63e178ac111daf68b8a368ce85
kosaci <kosacimadri@gmail.com>
2023-10-02 11:16:20 +0530

    Adding Comments File

---
+++ comments.cpp
@@ -0,0 +1,42 @@
+
+@creds Scaler.com 
+
+Comments are non executable statemnets. They are not executed by the compiler or interpreter.
+They are two types of Comments
+(a) Single Line
+(b) Multi line
+We use the comments to explain the function desc, parameters,etc. They make code understand not only for us when we revisit it after
+some time but also for everyone try to read the code.
+
+(a) Single Line comments
+
+These commenents are represented by //
+
+```
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Declaring a int varibale
+    int a = 5;
+    //print the declared variable
+    cout<<a;
+    return 0;
+}
+```
+
+Multi line commnets are represented by /*.....*/
+
+```
+#include <iostream>
+using namespace std;
+
+int main() {
+    /*Declare a variable and 
+    print a variable in C++*/
+
+    int a=5;
+    cout<<a;
+    retrun 0;
+}
+```
